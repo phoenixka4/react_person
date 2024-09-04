ba2d13b9b3cb0cadf519185252d3482eade7a046
Dmitry Dukyn <Phoenixka4@gmail.com>
2024-09-04 18:49:05 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import '../../../src/App.scss';
+import React from 'react';
 
 export const Person = ({ person }) => {
-    return (
-        <section className="Person">
-            <h2 className="Person__name">My name is {person.name}</h2>
-            {person.age 
-            ? <p className="Person__age">I am {person.age}</p> 
-            : ''}
-            {person.isMarried 
-            ? <p className="Person__partner">{person.partnerName} is my {person.sex === 'm'
-            ? 'wife' 
-            : 'husband'}</p> 
-            : 'I am not married'}
-        </section>
-    )
-}
+  const getPartnerRole = sex => (sex === 'm' ? 'wife' : 'husband');
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+      {person.isMarried
+        ? <p className="Person__partner">{person.partnerName} is my {getPartnerRole(person.sex)}</p>
+        : <p className="Person__partner">I am not married</p>}
+    </section>
+  );
+};
