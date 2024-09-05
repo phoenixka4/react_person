cda26f9972929b11915a7814eee3177b8757632b
Dmitry Dukyn <Phoenixka4@gmail.com>
2024-09-05 19:12:25 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
       {person.isMarried ? (
         <p className="Person__partner">
           {person.partnerName} is my {getPartnerRole(person.sex)}
